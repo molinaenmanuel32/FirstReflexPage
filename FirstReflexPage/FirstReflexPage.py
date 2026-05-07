76da7bf65a2bf1ea14681644f8c2d5670d61dd9f
ledes <ledes@LAPTOP-0NPRRT60>
2026-05-07 00:00:19 -0400

    Contador en reflex

--- FirstReflexPage/FirstReflexPage.py
+++ FirstReflexPage/FirstReflexPage.py
@@ -1,29 +1,32 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
-
 from rxconfig import config
 
 
 class State(rx.State):
     """The app state."""
+    count: int = 0
+
+    @rx.event
+    def increment(self):
+        self.count += 1
+
+    @rx.event
+    def decrement(self):
+        self.count -= 1
 
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
+            rx.heading("Welcome to Emañoland!", size="9"),
+            rx.hstack(
+                rx.button("Decrement", color_scheme="ruby", on_click=State.decrement),
+                rx.heading(State.count, font_size="2em"),
+                rx.button("Increment", color_scheme="grass", on_click=State.increment),
+                spacing="4",
             ),
             spacing="5",
             justify="center",
